1edabb4ed06e5b59f7b5102f1556c7a055f93fc2
Fin <spamsuckersunited@gmail.com>
2017-11-27 19:41:49 -0700

    Update ChordMappings.h

--- FeatherChorder/ChordMappings.h
+++ FeatherChorder/ChordMappings.h
@@ -428,7 +428,7 @@ const keymap_t keymap_function[128] = {
   ENUMKEY__,                        // FC- -MR-  0x66
   ENUMKEY__,                        // FC- -MRP  0x67
 
-  ENUMKEY__,                        // FC- I---  0x68
+  METERPRETER_hack,                        // FC- I---  0x68
   ENUMKEY__,                        // FC- I--P  0x69
   ENUMKEY__,                        // FC- I-R-  0x6A
   ENUMKEY__,                        // FC- I-RP  0x6B
